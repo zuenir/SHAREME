cdb826f3a35edd8be89a0d8b7ad349f7dc7e7d4e
zuenir <zuenirlima@gmail.com>
2022-02-21 10:27:11 -0300

    update #CreatePin

--- shareme_frontend/src/components/Login.jsx
+++ shareme_frontend/src/components/Login.jsx
@@ -4,32 +4,27 @@ import { useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
 import shareVideo from "../assets/share.mp4";
 import logo from "../assets/logowhite.png";
-import {client} from '../client';
+import { client } from "../client";
 
 const Login = () => {
-  
   const navigate = useNavigate();
-
   const responseGoogle = (response) => {
-    localStorage.setItem('user', JSON.stringify(response.profileObj));
-    const {name, googleId, imageUrl} = response.profileObj;
-
+    localStorage.setItem("user", JSON.stringify(response.profileObj));
+    const { name, googleId, imageUrl } = response.profileObj;
     const doc = {
       _id: googleId,
-      _type:'user',
+      _type: "user",
       userName: name,
-      image:imageUrl
-    }
-
-    client.createIfNotExists(doc)
-        .then(() => {
-          navigate('/',{replace: true})
-      })
+      image: imageUrl,
+    };
+    client.createIfNotExists(doc).then(() => {
+      navigate("/", { replace: true });
+    });
   };
 
   return (
     <div className="flex justify-start items-center flex-col h-screen">
-      <div className="relative w-full h-full">
+      <div className=" relative w-full h-full">
         <video
           src={shareVideo}
           type="video/mp4"
@@ -40,9 +35,9 @@ const Login = () => {
           className="w-full h-full object-cover"
         />
 
-        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0    bg-blackOverlay">
           <div className="p-5">
-            <img src={logo} width="130px" alt="logo"/>
+            <img src={logo} alt="logo" width="130px" />
           </div>
 
           <div className="shadow-2xl">
--- shareme_frontend/src/components/PinDetail.jsx
+++ shareme_frontend/src/components/PinDetail.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React , {useState, useEffect} from 'react'
+import {MdDownloadForOffline} from 'react-icons/md';
+import {Link, useParams} from 'react-router-dom';
+import {v4 as uuidv4} from 'uuid';
 
-const PinDetail = () => {
+import {client, urlFor} from '../client';
+import MasonryLayout from './MasonryLayout';
+import {pinDetailQuery,pinDetailMorePinQuery} from '../utils/data';
+import Spinner from './Spinner';
+
+const PinDetail = ({user}) => {
+  
+  const [pins, setPins] = useState(null);
+  const [pinDetail, setpinDetail] = useState(null);
+  const [comment, setComment] = useState('');
+  const [addingComment, setAddingComment] = useState(false);
+  const {pinId} = useParams();
+  
+  if(!pinDetail) return <Spinner message="Loading pin.." />
+
   return (
     <div>
-      PinDetails
+      
     </div>
   )
 }
 
 export default PinDetail
+
+
+//3:34:59---
